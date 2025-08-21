93584fb165869488d56b2c1f86816b60f4ba0b31
Mamadou Yacine Gueye <momogueye7@gmail.com>
2025-08-21 02:23:30 +0000

    Changement code backend et MAJ manifests

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 # Récupération des informations de connexion à la base de données depuis les variables d'environnement
 db_user = os.getenv('MYSQL_USER')
 db_password = os.getenv('MYSQL_PASSWORD')
@@ -15,6 +15,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-CORS(app)
 
 from app import routes
